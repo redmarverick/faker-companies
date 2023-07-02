31936ecdc6ed2d660ac27ede2fa6f60087a73d39
Nylbert Victor <nyldrift@hotmail.com>
2023-07-02 01:36:45 -0300

    linters correction

--- src/tests/details.test.js
+++ src/tests/details.test.js
@@ -1,11 +1,5 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import { useParams } from 'react-router-dom';
 import { getImagePath } from '../components/CountryItem';
-import Details from '../components/Details';
-import { fetchCompaniesByCountry, selectCompanies } from '../store/companiesSlice';
 
 // Tests that the correct image path is returned based on ISO code
 it('test_return_image_path_based_on_ISO_code', () => {
